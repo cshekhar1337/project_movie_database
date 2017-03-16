7d48770b707418eefef0e9c37b06d3f0cf42f00f
cshekhar1337 <cshekhar@Chandras-MacBook-Pro.local>
2017-03-16 11:12:04 -0700

    first commit

---
+++ media.py
@@ -0,0 +1,11 @@
+''' Class that defines Movie object. Every frame shown on the website is made of Movie object'''
+class Movie():
+	
+	def __init__(self, title, poster_image_url, trailer_youtube_url):
+		self.title = title
+		self.poster_image_url = poster_image_url
+		self.trailer_youtube_url = trailer_youtube_url
+
+
+
+
